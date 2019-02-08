9e8916f6ce0243392b2a7e75bcb1a9096db88f6c
Omotayo <tayoroyewole@gmail.com>
2019-02-08 17:18:00 -0500

    Added a toolbar to the main activity

--- app/src/main/java/com/example/tayor/karz/MainActivity.java
+++ app/src/main/java/com/example/tayor/karz/MainActivity.java
@@ -6,17 +6,19 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.tayor.karz.Model.Car;
+
 
 public class MainActivity extends AppCompatActivity implements
         CarFragment.OnListFragmentInteractionListener,BookCarFragment.OnFragmentInteractionListener,PaymentFragment.OnPaymentFragmentInteractionListener {
 
     private TextView mTextMessage;
+    private android.support.v7.widget.Toolbar mTopToolbar;
     private CardView cardView;
     private FrameLayout frameLayout;
 
@@ -76,7 +78,9 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // frameLayout = findViewById(R.id.frame2);
+
+        mTopToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mTopToolbar);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -84,8 +88,22 @@ public class MainActivity extends AppCompatActivity implements
 
         cardView = findViewById(R.id.card);
         //showFragment();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_car_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.profile:
+                Toast.makeText(this, "profile selected", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
