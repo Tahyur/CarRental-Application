dd84eed40a1edb2fb14c9e31cfe51251d7a24aef
Sarthak Chavan <sarthakchavan290195@gmail.com>
2019-02-09 19:55:05 -0600

    The title of actionbar changes, based on the navigation

--- app/src/main/java/com/example/deeppatel/car_rerntal/Home.java
+++ app/src/main/java/com/example/deeppatel/car_rerntal/Home.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
     private TextView mTextMessage;
+    android.support.v7.app.ActionBar actionBar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,20 +25,27 @@ public class Home extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                    // mTextMessage.setText(R.string.title_home);
+
+                    actionBar.setTitle(R.string.title_home);
+
                     return true;
                 case R.id.navigation_customer:
                     //mTextMessage.setText(R.string.title_customer);
+                    actionBar.setTitle(R.string.title_customer);
                     return true;
                 case R.id.navigation_car:
                     //mTextMessage.setText(R.string.title_car);
+                    actionBar.setTitle(R.string.title_car);
                     return true;
 
                 case R.id.navigation_rent:
                     //mTextMessage.setText(R.string.string_rent);
+                    actionBar.setTitle(R.string.string_rent);
                     return true;
 
                 case R.id.navigation_return_car:
                     //mTextMessage.setText(R.string.return_car);
+                    actionBar.setTitle(R.string.return_car);
                     return true;
 
             }
@@ -51,6 +61,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        actionBar = getSupportActionBar();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
